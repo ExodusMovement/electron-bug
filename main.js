03a32c4adb5afa6c48f6e3aac1329c2ce7b4b742
Ryan Zimmerman <opensrc@ryanzim.com>
2018-03-17 16:50:58 -0400

    WIP to reproduce

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let secondaryWindow
 
 function createWindow () {
   // Create the browser window.
@@ -41,11 +42,7 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
 
 app.on('activate', function () {
@@ -56,5 +53,16 @@ app.on('activate', function () {
   }
 })
 
+app.on('browser-window-created', (event, browserWindow) => {
+  browserWindow.on('close', event => {
+    event.preventDefault()
+    setTimeout(browserWindow.hide.bind(browserWindow), 250)
+  })
+})
+
+app.on('before-quit', function () {
+  setTimeout(() => BrowserWindow.getAllWindows().forEach(bw => bw.destroy()), 5000)
+  setImmediate(app.exit.bind(app))
+})
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
